f2c4b883c5f54214114d8dd17e138d965833b183
ZhuMX <zhumingxin4@163.com>
2019-03-29 16:49:43 +0800

    数据库操作相关

---
+++ test.py
@@ -0,0 +1,12 @@
+# table_data = {'a':"1",'b':'2'}
+# for key in table_data.keys():
+#     print(key)
+# key = ','.join(table_data.keys())
+# value = ','.join(table_data.values())
+# print(key)
+# print(value)
+# real_sql = "insert into"+ 'sss' +"("+ key +") values("+ value +");"
+# print(real_sql)
+
+import time
+print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
